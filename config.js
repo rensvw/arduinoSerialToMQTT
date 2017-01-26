b79a70e5e48ae389b6b965b3b675289767c021f0
Rens van Woudenberg <rensvanw@outlook.com>
2017-01-26 11:21:08 +0100

    changed hostname mqtt server

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ config.baudrate = process.env.BAUD_RATE || 9600;
 config.mqtt = {};
 config.mqtt.namespaceOutput = process.env.MQTT_NAMESPACE_OUTPUT || 'legoHouseWoonkamerOutput';
 config.mqtt.namespaceInput = process.env.MQTT_NAMESPACE_INPUT || 'legoHouseWoonkamerInput';
-config.mqtt.hostname = process.env.MQTT_HOSTNAME || 'test.mosquitto.org';
+config.mqtt.hostname = process.env.MQTT_HOSTNAME || '94.211.191.154';
 config.mqtt.port = process.env.MQTT_PORT || 1883;
 
 module.exports = config;
