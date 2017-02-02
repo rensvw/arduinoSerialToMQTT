2b44d2acf21707f1b828a06b4f785cf0111f15d5
Mike van Donselaar <mikevandonselaar@outlook.com>
2017-02-02 13:12:15 +0100

    Renewed version with all sensors initiated

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,44 +1,40 @@
-// Define integers settings for Arduino
+#include <OneWire.h> // Include Onewire for the digital Temperature Sensor
 
-int calibrationTime = 5; //the time we give the sensor to calibrate (10-60 secs according to the datasheet)
-int armTimeOut = 3; // the time before the alarm is activated
-int disableTimeOut = 5; // The time a user has to disable the alarm
-int alarmState = 0; // alarmState 0 = Residents are present, alarmState 1 =  Residents are absent
-boolean armed = false; // set default armed state
-
-// Define variables for the constant DIGITAL OUTPUT PINS on the Arduino
+// Define variables for the constant OUTPUT PINS on the Arduino
 // See documentation for detailed description of sensors
 
 //---------------Sensors------------//
-const int motionSensor = 2;    // Motion sensor
-const int lightSensor = 2; // Light Sensor
-const int tempSensor = 6; // Temparature Sensors
-const int proxSensor = 9; // Proximity Sensor
+//const int motionSensor = 2;    // Motion sensor
+const int delayTime = 300;
+const int lightSensor = A0; // Light Sensor
+OneWire ds(2); // Temparature Sensors
+const int proxSensor = 3; // Proximity Sensor
 const int doorSensor = 4; // Door contact
-const int windowSensor = 5; // Window contact
+const int windowSensor = 7; // Window contact
 
-const int activateButton = 10; // Alarm activation button
-const int deactivateButton = 12; // Alarm deactivation button
+// Motion is being measured with a Distance sensor, so that when the Lego House is being moved(while there is no motion inside, the alarm won't go off)
+const int trigPin = 9;
+const int echoPin = 8;
 
 //---------------Actuators----------//
-const int buzzer = 6; // Buzzer that is activated when the timeOut phase is activated
-const int siren = 7; // Siren when alarm is activated
-const int lightsLivingRoom = 8; // Lights for the livingroom
-const int heating = 9; // A heating device simulated by a Red Light
+const int buzzer = 88; // Buzzer that is activated when the timeOut phase is activated
+
+const int lightsLivingRoom = 11; // Lights for the livingroom
+const int heating = 12; // A heating device simulated by a Red Light
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
   // Set PINMODE for sensors
-  pinMode(motionSensor, INPUT_PULLUP);
   pinMode(lightSensor,INPUT);
   pinMode(tempSensor,INPUT);
   pinMode(proxSensor,INPUT);
-  pinMode(doorSensor,INPUT);
-  pinMode(windowSensor,INPUT);
+  pinMode(doorSensor,INPUT_PULLUP);
+  pinMode(windowSensor,INPUT_PULLUP);
 
-  pinMode(activateButton,INPUT);
-  pinMode(deactivateButton,INPUT);  
+  // Set PINMODE for Distance sensor
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 
   // Set PINMODE for Actuators
   pinMode(buzzer, OUTPUT);
@@ -46,22 +42,13 @@ void setup() {
   pinMode(lightsLivingRoom, OUTPUT);
   pinMode(heating, OUTPUT);
 
-  // Initialize low status for all sensors
+  // Initialize low status for Buzzer
   digitalWrite(buzzer,LOW);
-  digitalWrite(motionSensor, LOW);
-  digitalWrite(siren, LOW);
-
-  // Calibrate sensor so that it doesn't immediately activates itself
-  calibrateSensor();
-  }  
 }
 
 void loop() {
     // Read States of Sensors
-    int motionSensorState = digitalRead(motionSensor);
-    //lightSensorState = digitalRead(lightSensor);
-    // tempSensorState = digitalRead(tempSensor); // Code that reads Temperature sensor
-    // proxSensorState = readIR(5);               // Code that reads Proximity sensor
+    int lightSensorState = analogRead(lightSensor);            
     int doorSensorState = digitalRead(doorSensor);
     int windowSensorState = digitalRead(windowSensor);
 
@@ -71,32 +58,58 @@ void loop() {
     int lightsLivingRoomState = digitalRead(lightsLivingRoom);
     int heatingState = digitalRead(heating);
     
+    //Run Code for distance sensor that detects motion
+    long duration, distance;
+    digitalWrite(trigPin, LOW); 
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+    
+    //Run code for temperature sensor
+    byte data[2];
+    ds.reset(); 
+    ds.write(0xCC);
+    ds.write(0x44);
+    delay(750);
+    ds.reset();
+    ds.write(0xCC);
+    ds.write(0xBE);
+    data[0] = ds.read(); 
+    data[1] = ds.read();
+    int Temp = (data[1]<<8)+data[0];
+    Temp = Temp>>4;
+    
+    
     // Define message
     String message = Serial.readStringUntil('#');  
+    if(message == "lightOn"){
+      digitalWrite(lightsLivingRoom, HIGH);   // Turn the LED on 
+      Serial.println("{'lamp':'"+ String(digitalRead(lightsLivingRoom)) + "','heating':'"+ String(heatingState) + "','movingSensor':'"+ String(distance) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(Temp) + "','lightSensor':'" + String(lightSensorState) + "'}");
+      delay(delayTime); 
+    }
+    if(message == "lightOff"){  
+      digitalWrite(lightsLivingRoom, LOW);    // Turn the LED off
+      Serial.println("{'lamp':'"+ String(digitalRead(lightsLivingRoom)) + "','heating':'"+ String(heatingState) + "','movingSensor':'"+ String(distance) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(Temp) + "','lightSensor':'" + String(lightSensorState) + "'}");
+      delay(delayTime); 
+    }
 
-   if(message == "lightOn"){
-    digitalWrite(lightsLivingRoom, HIGH);   // Turn the LED on 
-    Serial.println("{'lamp':'1','heating':'"+ String(heatingState) + "','movingSensor':'"+ String(motionSensorState) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(tempSensorState) + "','lightSensor':'" + String(lightSensorState) + "'}");
-    delay(1000); 
-  }
-   if(message == "lighOff"){  
-    digitalWrite(lightsLivingRoom, LOW);    // Turn the LED off
-    Serial.println("{'lamp':'0','heating':'"+ String(heatingState) + "','movingSensor':'"+ String(motionSensorState) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(tempSensorState) + "','lightSensor':'" + String(lightSensorState) + "'}");
-    delay(1000); 
-  }
+    if(message == "heatingOn"){
+      digitalWrite(heating, HIGH); // Simulate the heating by turning the red light on
+      Serial.println("{'lamp':'"+ String(lightsLivingRoomState) + "','heating':'"+ String(digitalRead(heating)) + "','movingSensor':'"+ String(distance) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(Temp) + "','lightSensor':'" + String(lightSensorState) + "'}");
+      delay(delayTime); 
+    }
 
-  if(message == "heatingOn"){
-    digitalWrite(heating, HIGH); // Simulate the heating by turning the red light on
-    Serial.println("{'lamp':'"+ String(lightsLivingRoomState) + "','heating':'"+ String(heatingState) + "','movingSensor':'"+ String(motionSensorState) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(tempSensorState) + "','lightSensor':'" + String(lightSensorState) + "'}");
-    delay(1000); 
-  }
+    if(message == "heatingOff"){
+      digitalWrite(heating, LOW); // Simulate the heating by turning the red light on
+      Serial.println("{'lamp':'"+ String(lightsLivingRoomState) + "','heating':'"+ String(digitalRead(heating)) + "','movingSensor':'"+ String(distance) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(Temp) + "','lightSensor':'" + String(lightSensorState) + "'}");
+      delay(delayTime); 
+    }
 
-  if(message == "heatingOff"){
-    digitalWrite(heating, LOW); // Simulate the heating by turning the red light on
-    Serial.println("{'lamp':'"+ String(lightsLivingRoomState) + "','heating':'"+ String(heatingState) + "','movingSensor':'"+ String(motionSensorState) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(tempSensorState) + "','lightSensor':'" + String(lightSensorState) + "'}");
-    delay(1000); 
-  }
+     Serial.println("{'lamp':'"+ String(digitalRead(lightsLivingRoom)) + "','heating':'"+ String(digitalRead(heating)) + "','movingSensor':'"+ String(distance) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(Temp) + "','lightSensor':'" + String(lightSensorState) + "'}");
 
-  Serial.println("{'lamp':'"+ String(lightsLivingRoomState) + "','heating':'"+ String(heatingState) + "','movingSensor':'"+ String(motionSensorState) + "','doorSensor':'"+ String(doorSensorState) + "','windowSensor':'" + String(windowSensorState) + "','tempSensor':'" + String(tempSensorState) + "','lightSensor':'" + String(lightSensorState) + "'}");
-  delay(1000);
+     delay(delayTime);
+    
 } 
